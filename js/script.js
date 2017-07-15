96aecde62334bcd3072df689b85ba939efb172b6
WENDY MARTELL <wendymartelll@gmail.com>
2017-07-14 17:12:42 -0700

    Move app working including wikipedia and NYT api's

--- js/script.js
+++ js/script.js
@@ -28,8 +28,45 @@ function loadData() {
     var nytimesURL ='http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStr +'&sort=newest&api-key=92a038c1b4bd46c39f544d5ca894e230';
 
     $.getJSON(nytimesURL, function (data){
-      console.log(data);
-    })
+      $nytHeaderElem.text ('New York Times about' + cityStr);
+
+      articles = data.response.docs;
+      for (var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class = "article">' + '<a href = "'+ article.web_url +'">'
+        + article.headline.main + '</a>' + '<p>' +article.snippet + '</p>' +'</li>');
+      };
+
+    }).error(function (e) {
+      $nytHeaderElem.text ('New York Times about could not be loaded' );
+    });
+
+    // Wikipedia AJAX request is here
+    var wikiURL = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+
+    var wikiRequestTimeout = setTimeout(function(){  // protection for error
+      $wikiElem.text("failed to get wikipedia resources");
+    },8000);
+
+    $.ajax({
+      url: wikiURL,
+      dataType:"jsonp",
+
+      success: function(response){
+        var articleList= response[1];
+
+        for (var i = 0; i < articleList.length; i++) {
+          var articleStr = articleList[i];
+          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+          $wikiElem.append('<li><a href = "'+ url +'">'
+          + articleStr + '</a></li>');
+        };
+        clearTimeout(wikiRequestTimeout);
+      }
+    });
+
+
+
     return false;
 };
 
